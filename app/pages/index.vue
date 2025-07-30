82c62c145c20051c2a76def85fa769d008c168be
Kazuma Kusu <xkzm@camphora.tech>
2025-07-30 17:33:24 +0900

    chore: Remove unnecessary components in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -30,18 +30,6 @@ useSeoMeta({
     </UPageHero>
 
     <UPageSection
-      v-for="(section, index) in page.sections"
-      :key="index"
-      :title="section.title"
-      :description="section.description"
-      :orientation="section.orientation"
-      :reverse="section.reverse"
-      :features="section.features"
-    >
-      <ImagePlaceholder />
-    </UPageSection>
-
-    <UPageSection
       :title="page.features.title"
       :description="page.features.description"
     >
@@ -53,30 +41,6 @@ useSeoMeta({
           spotlight
         />
       </UPageGrid>
-    </UPageSection>
-
-    <UPageSection
-      id="testimonials"
-      :headline="page.testimonials.headline"
-      :title="page.testimonials.title"
-      :description="page.testimonials.description"
-    >
-      <UPageColumns class="xl:columns-4">
-        <UPageCard
-          v-for="(testimonial, index) in page.testimonials.items"
-          :key="index"
-          variant="subtle"
-          :description="testimonial.quote"
-          :ui="{ description: 'before:content-[open-quote] after:content-[close-quote]' }"
-        >
-          <template #footer>
-            <UUser
-              v-bind="testimonial.user"
-              size="lg"
-            />
-          </template>
-        </UPageCard>
-      </UPageColumns>
     </UPageSection>
 
     <USeparator />
