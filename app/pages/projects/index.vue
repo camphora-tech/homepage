705d735f8348b31906a72af2a8cfefa8675f5d41
Kazuma Kusu <xkzm@camphora.tech>
2025-08-31 23:15:36 +0900

    fix: Projects page
    - UColorModeImage requires light and dark props

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -26,14 +26,19 @@
           class="overflow-hidden">
           <div class="flex flex-col md:flex-row">
             <div class="w-full md:w-1/2 aspect-[4/3]">
-              <UColorModeImage :src="project.image.src" :alt="project.title" class="w-full h-full object-cover" />
+              <UColorModeImage :light="project.image.src" :dark="project.image.src" :alt="project.title"
+                class="w-full h-full object-cover" />
             </div>
             <div class="mt-4 md:mt-0 md:px-6 md:w-1/2 flex flex-col">
-              <h2 class="text-xl font-bold">{{ project.title }}</h2>
+              <h2 class="text-xl font-bold">
+                {{ project.title }}
+              </h2>
               <div class="flex flex-wrap gap-2 pt-2">
                 <UBadge v-for="(tag, tagIndex) in project.tags" :key="tagIndex" :label="tag" variant="soft" />
               </div>
-              <p class="mt-2 text-neutral-500 dark:text-neutral-400">{{ project.description }}</p>
+              <p class="mt-2 text-neutral-500 dark:text-neutral-400">
+                {{ project.description }}
+              </p>
             </div>
           </div>
         </UPageCard>
